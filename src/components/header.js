172899ca4f72759915ebde07acea241bb69562e8
Arda Özcan <arda.oezcan@aroundhome.de>
2020-05-26 20:33:54 +0200

    changed conditions in header

--- src/components/header.js
+++ src/components/header.js
@@ -17,16 +17,30 @@ const Header = ({ children, data }) => {
     <div className="header-container">
       <header className="header">
         <div className="desktop-navbar">
-          <div className="header-video-container"><video loop="1" width="100%" height="250" controls="" autoPlay autoplay="1" name="media" src="http://54499171.swh.strato-hosting.eu/bipolarjs/wp-content/uploads/2020/04/Sequenz-03_11.mp4" type="video/mp4" ></video></div>
-          <Navbar data={data} />
+          <div className="header-video-container">
+            <video
+              loop="1"
+              width="100%"
+              height="250"
+              controls=""
+              autoPlay
+              autoplay="1"
+              name="media"
+              src="http://54499171.swh.strato-hosting.eu/bipolarjs/wp-content/uploads/2020/04/Sequenz-03_11.mp4"
+              type="video/mp4">
+            </video>
+          </div>
+          {data && <Navbar data={data} />}
         </div>
         <div className="mobile-navbar">
           <div className="header-mobile-container">
             <div className="header-mobile-column-left"></div>
-            <div className="header-mobile-column-middle"><img className="header-mobile-logo" src="http://54499171.swh.strato-hosting.eu/bipolarjs/wp-content/uploads/2020/04/bipolar_logo-0.svg" alt=""></img></div>
+            <div className="header-mobile-column-middle">
+              <img className="header-mobile-logo" src="http://54499171.swh.strato-hosting.eu/bipolarjs/wp-content/uploads/2020/04/bipolar_logo-0.svg" alt="" />
+            </div>
             <div className="header-mobile-column-right"><DrawerToggleButton click={drawerToggleClickHandler} /></div>
           </div>
-          {<SideDrawer data={data} click={backdropClickHandler} show={sideDrawerOpen} />}
+          {data && <SideDrawer data={data} click={backdropClickHandler} show={sideDrawerOpen} />}
           {sideDrawerOpen && <Backdrop click={backdropClickHandler} />}
         </div>
       </header>
